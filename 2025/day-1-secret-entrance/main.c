b702e8566999740e5fa2e76b61ed3a88ced9e25f
sage-etcher <sage.signup@email.com>
2025-12-10 15:31:01 -0500

    part 2 complete

--- 2025/day-1-secret-entrance/main.c
+++ 2025/day-1-secret-entrance/main.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
 int
 main (void)
 {
@@ -13,9 +15,14 @@ main (void)
     uint32_t magnitude = 0;
 
     int32_t movement = 0;
+
+    int32_t a = 0;
+    int32_t b = 0;
     int32_t position = 50;
+    int32_t new_position = 0;
 
     uint32_t real_code = 0;
+
 
     /* open the input file */
     fp = fopen ("input", "r");
@@ -30,12 +37,27 @@ main (void)
             movement *= -1;
         }
 
-        /* check if at 0 */
-        position += movement;
-        if (!(position % 100))
+        /* check if at, or past, 0 */
+        a = position + movement;
+        b = ABS (a);
+
+        real_code += b / 100;
+        new_position = b % 100;
+
+        if ((a <= 0) && (position > 0))
         {
             real_code++;
         }
+
+        if ((a < 0) && (new_position != 0))
+        {
+            new_position = 100 - new_position;
+        }
+
+        position = new_position;
+
+        printf ("%c%-3d %4d %2d %2d\n", direction, magnitude, movement, position, real_code);
+
     } while (!feof (fp));
 
     printf ("real_code: %u\n", real_code);
