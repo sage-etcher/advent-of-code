de1db45460016fb613dbe554e52df3b9cf8416d6
sage-etcher <sage.signup@email.com>
2025-12-15 08:50:49 -0500

    day 5 part 2 unfinished, will comeback to

--- 2025/day-5-cafeteria/main.c
+++ 2025/day-5-cafeteria/main.c
@@ -15,11 +15,11 @@ main (int argc, char **argv)
 
     ivec2_t oh = { 0 };
     int rc = 0;
-    size_t i = 0;
+    size_t ii = 0;
+    size_t jj = 0;
     ivec2_t *ranges = NULL;
     size_t range_cnt = 0;
-    
-    uint64_t id = 0;
+
     int sum = 0;
 
     assert (argc >= 2);
@@ -40,21 +40,34 @@ main (int argc, char **argv)
     assert (ranges != NULL);
 
     fseek (fp, 0L, SEEK_SET);
-    for (i = 0; i < range_cnt; i++)
+    for (ii = 0; ii < range_cnt; ii++)
     {
-        rc = fscanf (fp, "%llu-%llu\n", &ranges[i].x, &ranges[i].y);
+        rc = fscanf (fp, "%llu-%llu\n", &ranges[ii].x, &ranges[ii].y);
     }
 
-    
-    while ((rc = fscanf (fp, "%llu\n", &id)) == 1) 
-    {
-        for (i = 0; i < range_cnt; i++)
-        {
-            if (id < ranges[i].x) continue;
-            if (id > ranges[i].y) continue;
+    fclose (fp);
+    fp = NULL;
 
-            sum++;
-            break;
+    for (ii = 0; ii < range_cnt; ii++)
+    {
+        ivec2_t *r0 = &ranges[ii];
+        for (jj = 0; jj < range_cnt; jj++)
+        {
+            if (ii == jj) continue;
+
+            ivec2_t *r1 = &ranges[jj];
+            ivec2_t c = { 0 };
+
+            if ((r1->x <= r0->x) && (r0->x <= r1->y))
+            {
+            }
+
+            // 2-130
+            // 14-18
+            // if x1 < x2 && y1 > y2: remove 2
+            // if 
+
+
         }
     }
 
@@ -62,7 +75,6 @@ main (int argc, char **argv)
 
 
     free (ranges);
-    fclose (fp);
     return 0;
 }
 /* end of file */
